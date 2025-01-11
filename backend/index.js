b42cea67415d48e6e9ddda59ed2b152f9e961bb2
anuja-0803 <anujashinde@sdlccorp.com>
2025-01-11 11:49:19 +0530

    created Routes

--- backend/index.js
+++ backend/index.js
@@ -2,3 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 8080;
+
+const AuthRouter = require("./Routes/Authrouter");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`port is running on ${PORT}`);
+});
+
+app.use("/auth", AuthRouter);
