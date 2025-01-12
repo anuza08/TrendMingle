9f0ab37c34d22d6e952c7a747d3135992626ac15
anuja-0803 <anujashinde@sdlccorp.com>
2025-01-12 19:32:45 +0530

    Added temp home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import Home from "./Pages/Home";
 function App() {
   useEffect(() => {
     const link = document.createElement("link");
@@ -14,9 +15,10 @@ function App() {
   return (
     <>
       <div>
-        <h1>Authentication Application</h1>
+        {/* <h1>Authentication Application</h1> */}
         <Routes>
-          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
---
+++ frontend/src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      {" "}
+      <h1 className="text-4xl  mb-12 text-center font-playfair italic">
+        Welcome to TrendMingle
+      </h1>
+    </>
+  );
+};
+
+export default Home;
